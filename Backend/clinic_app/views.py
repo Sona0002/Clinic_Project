ab33b5c6ea129c93d20539a29439bd9195a701d8
Sona0002 <140248044+Sona0002@users.noreply.github.com>
2025-08-09 20:23:12 +0530

    Update views.py

--- Backend/clinic_app/views.py
+++ Backend/clinic_app/views.py
@@ -10,8 +10,6 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
 
-
-# User profile
 class UserProfileView(generics.RetrieveAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -20,20 +18,17 @@ class UserProfileView(generics.RetrieveAPIView):
         return self.request.user
 
 
-# Doctor list
 class DoctorListView(generics.ListAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
     permission_classes = [permissions.AllowAny]
 
 
-# Appointment create
 class AppointmentCreateView(generics.CreateAPIView):
     serializer_class = AppointmentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
-# Appointment list (current user only)
 class AppointmentListView(generics.ListAPIView):
     serializer_class = AppointmentSerializer
     permission_classes = [permissions.IsAuthenticated]
